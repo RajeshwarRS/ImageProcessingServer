5dddc72402c3c829f3f0e74e93001bbdaa9649d7
Rajeshwar <rajeshwar.r.shinde1091998@gmail.com>
2022-02-13 11:00:00 +0530

    project

---
+++ api/app.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const path=require('path');
+const fs=require('fs');
+const app=express();
+const pathToIndex=path.resolve(__dirname,'../client/index.html');
+const router = require('./src/router');
+app.use('/',router);
+app.use(express.static(path.resolve(__dirname,'uploads')));
+app.use('/*',(request,response)=>{
+
+response.sendFile(pathToIndex);
+})
+
+
+module.exports=app
